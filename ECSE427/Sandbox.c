77527109c1ac3bfe9fec36f17025ca267c30fa77
chiwingsit <chiwingsit@gmail.com>
2014-02-04 21:04:24 -0500

    Add MemoryLayout.txt and fix stack and heap in Sandbox

--- ECSE427/Sandbox.c
+++ ECSE427/Sandbox.c
@@ -7,23 +7,40 @@ extern char data_start, edata, end;
 
 int main(int argc, char *argv[])
 {
-	unsigned int i;
-	void* buffer = 0;
+	unsigned int size_heap;
+	unsigned int size_stack = 1;
 	
-	for(i = 2900000000; 1; i = i + 1000){
-		buffer = malloc(i);
-		printf("ADDRESS: %p", buffer);
-		printf("SIZE : %u\n", i);
+	FILE *f;
+	
+	void* buffer,*heap_start, *stack_start;
+	buffer = malloc(1);
+	heap_start = buffer;
+	
+	for(size_heap = 1024*1024*25; 1; size_heap += 1024*1024){
+		buffer = (void*) malloc(size_heap);
 		if(buffer == NULL)
 			break;
 		free(buffer);
 	}
-	printf("Section		Start		End		Size\n");
-	printf("text		%p	%p	%#x\n", &_start, &_fini, &_fini-&_start);
-	printf("data		%p	%p	%#x\n", &data_start, &edata, &edata-&data_start);
-	printf("bss 		%p	%p	%#x\n", &edata, &end, &end-&edata);
+
+	stack_start = alloca(size_stack);
 	
-	printf("SIZE : %u\n", i);
+	while(1){
+		alloca(size_stack);
+		size_stack++;
+		
+		f = fopen("MemoryLayout.txt", "w");
+		
+		fprintf(f,"Section		Start		End		Size\n");
+		fprintf(f,"text		%p	%p	%#x\n", &_start, &_fini, &_fini-&_start);
+		fprintf(f,"data		%p	%p	%#x\n", &data_start, &edata, &edata-&data_start);
+		fprintf(f,"bss 		%p	%p	%#x\n", &edata, &end, &end-&edata);
+		fprintf(f,"heap 		%p	%p	%#x\n", heap_start, heap_start + size_heap, size_heap);
+		fprintf(f,"stack		%p	%p	%#x\n", stack_start, stack_start - size_stack, size_stack);
+
+		fclose(f);
+	}
+	
 	exit(EXIT_SUCCESS);
 		
 }
