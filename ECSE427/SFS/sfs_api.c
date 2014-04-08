ed1b241760c59c4a423fd0e4122c0756c057d04b
chiwingsit <chiwingsit@gmail.com>
2014-04-08 00:31:26 -0400

    Commented some of the code

--- ECSE427/SFS/sfs_api.c
+++ ECSE427/SFS/sfs_api.c
@@ -8,6 +8,7 @@
 #define MAX_FD 125
 #define MAX_BLOCK 1001
 
+// Superblock struct
 typedef struct{
 	int rootBlocks;
 	int fatBlocks;
@@ -32,7 +33,7 @@ typedef struct{
 // Represent an entry in file access table
 typedef struct{
 	int dbIndex;
-	int next; //Next fat index
+	int next;
 }fat_entry;
 
 super_block superBlock;
@@ -47,23 +48,28 @@ int mksfs(int fresh){
 	blockSize = (sizeof(fdt) > sizeof(fat) ? sizeof(fdt) : sizeof(fat));
 	int i;
 	if(fresh){
-		//initialise a new FS called FileSystem.
+		// Initialise a new FS called FileSystem.
 		init_fresh_disk("FileSystem", blockSize, MAX_BLOCK);
 		for(i = 0; i < MAX_FD; i++){
+			// Initialize the root values
 			strcpy(root[i].fileName, "");
 			root[i].size = 0;
 			root[i].fatIndex = -1;
 			
+			// Initialize the fdt values
 			fdt[i].fatIndex = -1;
 			fdt[i].readIndex = 0;
 			fdt[i].writeIndex = 0;
 		}
 		for(i = 0; i < MAX_BLOCK; i++){
+			// Initialize the fat values
 			fat[i].dbIndex = -2;
 			fat[i].next = -1;
+			// Set all free blocks to empty
 			freeBlocks[i] = 0;
 		}
 
+		// Values for the super block
 		superBlock.rootBlocks = 1;
 		superBlock.fatBlocks = 1;
 		superBlock.dataBlocks = 0;
@@ -74,12 +80,14 @@ int mksfs(int fresh){
 		freeBlocks[2] = 1;
 		freeBlocks[MAX_BLOCK - 1] = 1;
 		
+		// Update the disk memory
 		write_blocks(0, 1, (void *) &superBlock);
 		write_blocks(1, 1, (void *) &root);
 		write_blocks(2, 1, (void *) &fat);
 		write_blocks(MAX_BLOCK - 1, 1, (void *) &freeBlocks);
 	}
 	else{
+		// Else get the data from the previous file system
 		init_disk("FileSystem", blockSize, MAX_BLOCK);
 		
 		read_blocks(0, 1, (void *) &superBlock);
@@ -91,7 +99,7 @@ int mksfs(int fresh){
 	return 0;
 }
 
-
+// Get next free block index
 int nextFreeBlock(){
 	int i;
 	for(i = 0; i < MAX_BLOCK; i++){
@@ -101,6 +109,7 @@ int nextFreeBlock(){
 	return 0;
 }
 
+// Get next free root index
 int nextFreeRoot(){
 	int i;
 	for(i = 0; i < MAX_FD - 1; i++){
@@ -110,6 +119,7 @@ int nextFreeRoot(){
 	return 0;
 }
 
+// Get next free fat index
 int nextFreeFat(){
 	int i;
 	for(i = 0; i < MAX_BLOCK; i++){
@@ -123,6 +133,7 @@ void sfs_ls(void){
 	int i;
 	printf("File Name %20s\n", "Size");
 	printf("==========================================\n");
+	// Print all the files present in the root dir
 	for(i = 0; i < MAX_FD; i++){
 		if(root[i].fatIndex != -1)
 			printf("%s %20d B\n", root[i].fileName, root[i].size);
@@ -130,23 +141,28 @@ void sfs_ls(void){
 	printf("\n");
 }
 
+// Create a new file
 int sfs_fcreate(char *name){
 	int dirIndex = nextFreeRoot();
 	if(dirIndex == -1) return -1;
 	int fatIndex = nextFreeFat();
 	if(fatIndex == -1) return -1;
 	
+	// Put new file in root
 	strcpy(root[dirIndex].fileName, name);
 	root[dirIndex].size = 0;
 	root[dirIndex].fatIndex = fatIndex;
 	
+	// Update fat
 	fat[fatIndex].dbIndex = -1;
 	fat[fatIndex].next = -1;
 	
+	// Update fdt
 	fdt[dirIndex].fatIndex = fatIndex;
 	fdt[dirIndex].writeIndex = 0;
 	fdt[dirIndex].readIndex = 0;
 	
+	// Update the disk memory
 	write_blocks(1, 1, (void *) &root);
 	write_blocks(2, 1, (void *) &fat);
 	write_blocks(MAX_BLOCK - 1, 1, (void *) &freeBlocks);
@@ -155,9 +171,10 @@ int sfs_fcreate(char *name){
 }
 
 int sfs_fclose(int fileID){
+	// if file is not openned return 1
 	if(fdt[fileID].fatIndex == -1)
 		return 1;
-	else{
+	else{ // else updates the fdt and return 0
 		fdt[fileID].fatIndex = -1;
 		fdt[fileID].readIndex = 0;
 		fdt[fileID].writeIndex = 0;
@@ -183,72 +200,76 @@ int sfs_fopen(char *name){
 }
 
 int sfs_fwrite(int fileID, char *buf, int length){
-	if(fdt[fileID].fatIndex == -1) return 0;
+	if(fdt[fileID].fatIndex != -1){
 	
-	int tempLength = length;
-	int fatIndex = fdt[fileID].fatIndex;
-	
-	char temp_buffer[blockSize];
-	int offset = fdt[fileID].writeIndex;
-	
-	while(offset > blockSize){
-		fatIndex = fat[fatIndex].next;
-		offset = offset - blockSize;
+		int tempLength = length;
+		int fatIndex = fdt[fileID].fatIndex;
 		
-		if(fatIndex == -1) return 0;
-	}
-	int blockIndex = fat[fatIndex].dbIndex;
-	
-	// block index exist
-	if(blockIndex != -1){
-	
-		if(length > (blockSize - offset)){
-			read_blocks(blockIndex, 1, (void *) temp_buffer);
-			memcpy(temp_buffer + offset, buf, blockSize - offset);
-			write_blocks(blockIndex, 1, (void *) temp_buffer);
-			length = length - blockSize + offset;
-			buf = buf + blockSize - offset;
-		}
-		else{
-			read_blocks(blockIndex, 1, (void *) temp_buffer);
-			memcpy(temp_buffer + offset, buf, length);
-			write_blocks(blockIndex, 1, (void *) temp_buffer);
-			length = 0;
-			buf = buf + length;
-		}
-	}
-	while(length > 0){
-		int partSize = length > blockSize ? blockSize:length;
-		memcpy(temp_buffer, buf, partSize);
+		char temp_buffer[blockSize];
+		int offset = fdt[fileID].writeIndex;
 		
-		blockIndex = nextFreeBlock();
-		if(blockIndex == -1) return 0;
-		
-		if(fat[fatIndex].dbIndex != -1){
-			int nextFatIndex = nextFreeFat();
-			if(nextFatIndex == -1) return 0;
+		while(offset > blockSize){
+			fatIndex = fat[fatIndex].next;
+			offset = offset - blockSize;
 			
-			fat[fatIndex].next = nextFatIndex;
-			fatIndex = nextFatIndex;
+			if(fatIndex == -1) return 0;
+		}
+		int blockIndex = fat[fatIndex].dbIndex;
+		
+		// Block index already exists
+		if(blockIndex != -1){
+		
+			if(length > (blockSize - offset)){
+				read_blocks(blockIndex, 1, (void *) temp_buffer);
+				memcpy(temp_buffer + offset, buf, blockSize - offset);
+				write_blocks(blockIndex, 1, (void *) temp_buffer);
+				length = length - blockSize + offset;
+				buf = buf + blockSize - offset;
+			}
+			else{
+				read_blocks(blockIndex, 1, (void *) temp_buffer);
+				memcpy(temp_buffer + offset, buf, length);
+				write_blocks(blockIndex, 1, (void *) temp_buffer);
+				length = 0;
+				buf = buf + length;
+			}
+		}
+		while(length > 0){
+			int partSize = length > blockSize ? blockSize:length;
+			memcpy(temp_buffer, buf, partSize);
+			
+			blockIndex = nextFreeBlock();
+			if(blockIndex == -1) return 0;
+			
+			if(fat[fatIndex].dbIndex != -1){
+				int nextFatIndex = nextFreeFat();
+				if(nextFatIndex == -1) return 0;
+				
+				fat[fatIndex].next = nextFatIndex;
+				fatIndex = nextFatIndex;
+			}
+			
+			freeBlocks[blockIndex] = 1;
+			fat[fatIndex].dbIndex = blockIndex;
+			fat[fatIndex].next = -1;
+			
+			write_blocks(blockIndex, 1, (void *) temp_buffer);
+			length = length - partSize;
+			buf = buf + partSize;
 		}
 		
-		freeBlocks[blockIndex] = 1;
-		fat[fatIndex].dbIndex = blockIndex;
-		fat[fatIndex].next = -1;
+		root[fileID].size = root[fileID].size + tempLength;
+		fdt[fileID].writeIndex = root[fileID].size;
 		
-		write_blocks(blockIndex, 1, (void *) temp_buffer);
-		length = length - partSize;
-		buf = buf + partSize;
+		// Update the disk memory
+		write_blocks(1, 1, (void *) &root);
+		write_blocks(2, 1, (void *) &fat);
+		write_blocks(MAX_BLOCK - 1, 1, (void *) &freeBlocks);
+		
+		return tempLength;
 	}
 	
-	root[fileID].size = root[fileID].size + tempLength;
-	fdt[fileID].writeIndex = root[fileID].size;
-	
-	write_blocks(1, 1, (void *) &root);
-	write_blocks(2, 1, (void *) &fat);
-	write_blocks(MAX_BLOCK - 1, 1, (void *) &freeBlocks);
-	
-	return tempLength;
+	return 0;
 }
 
 int sfs_fread(int fileID, char *buf, int length){
@@ -307,6 +328,7 @@ int sfs_fread(int fileID, char *buf, int length){
 }
 
 int sfs_fseek(int fileID, int offset){
+	// if file is open change the read and write indices
 	if(fdt[fileID].fatIndex != -1){
 		fdt[fileID].writeIndex = offset;
 		fdt[fileID].readIndex = offset;
@@ -317,10 +339,11 @@ int sfs_fseek(int fileID, int offset){
 
 int sfs_remove(char *file){
 	int i;
-	for(i = 0; i < MAX_FD; i++){
+	for(i = 0; i < MAX_FD; i++){ // find the specific file
 		if(strcmp(root[i].fileName, file) == 0){
 			int fatIndex = root[i].fatIndex;
 			
+			// Remove iteratively all the fat entries
 			while(fat[fatIndex].next != -1){
 				int next;
 				freeBlocks[fat[fatIndex].dbIndex] = 0;
@@ -332,14 +355,17 @@ int sfs_remove(char *file){
 				fatIndex = next;
 			}
 			
+			// Remove the file from the root
 			strcpy(root[i].fileName,"");
 			root[i].size = 0;
 			root[i].fatIndex = -1;
 			
+			// Remove the file from the fdt
 			fdt[i].fatIndex = -1;
 			fdt[i].readIndex = 0;
 			fdt[i].writeIndex = 0;
 
+			// Update the disk memory
 			write_blocks(1, 1, (void *) &root);
 			write_blocks(2, 1, (void *) &fat);
 			write_blocks(MAX_BLOCK - 1, 1, (void *) &freeBlocks);
